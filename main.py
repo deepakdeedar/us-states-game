3513c29fc88b7a52e77b34ff0c4528fe7c1bd2de
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2020-12-24 15:19:47 +0530

    commit

--- main.py
+++ main.py
@@ -15,10 +15,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
 
     if answer_state == 'Exit':
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         data_set = pandas.DataFrame(missing_states)
         data_set.to_csv("more_to_learn.csv")
         break
